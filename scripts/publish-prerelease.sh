fd6ba7349545c0c6290b25690bed4c26a356f1d5
Yaroslav pavliuk <yarik-mac@Yaroslavs-MacBook-Air.local>
2024-09-07 12:54:30 +0300

    fix: add yarn install;

--- scripts/publish-prerelease.sh
+++ scripts/publish-prerelease.sh
@@ -21,6 +21,10 @@ tagName="v$publishVersion"
 echo "Setting package versions to: $publishVersion"
 ./node_modules/.bin/lerna version $publishVersion --exact --yes --no-git-tag-version --no-push --force-publish=\*
 
+# Install dependencies to update Yarn.lock
+echo "Updating yarn.lock file"
+yarn install
+
 # Add changes to Git
 echo "Committing version changes"
 git config user.name "github-actions"
